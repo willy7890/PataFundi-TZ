a0a85b62efd146cfee93fce2e40f6c61c8fb7615
mtundudev <dottomtundu9@gmail.com>
2026-09-08 19:11:35 +0300

    models to init

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,6 +4,10 @@ from app.core.database import Base
 from app.models.refreshtoken import RefreshToken
 from app.models.user import User
 from app.models.customer import Customer
+from app.models.district import District
+from app.models.regions import Region
+from app.models.street import Street
+from app.models.ward import Ward
 
 
-__all__ =["Base","RefreshToken","User","Customer"]
+__all__ =["Base","RefreshToken","User","Customer","Region","District","Street","Ward"]
